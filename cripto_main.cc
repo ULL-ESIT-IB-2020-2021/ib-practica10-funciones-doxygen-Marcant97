f65c891506014d73b5db15dd139dcf5f7c9b9b23
usuario <alu0101386560@ull.edu.es>
2020-12-13 21:37:48 +0000

    Cambios 1

---
+++ cripto_main.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+
+
+int k;
+std::string cadena1{};
+
+//Función encriptado de César
+char cesar(std::string cadena , int x){
+  for (unsigned int i=0; i<cadena.length(); i++){
+    cadena[i]+=x;
+    std::cout<<cadena[i];
+    }
+}
+
+
+int main(){
+    std::cout<<"Introduce k"<<std::endl;
+    std::cin>>k;
+    std::cout<<"Introduce cadena"<<std::endl;
+    std::cin>>cadena1;
+
+  std::ofstream myfile;
+    myfile.open ("example.txt");
+    myfile << cadena1;
+    myfile.close();
+
+//Encriptado César
+  std::cout<<cesar(cadena1, k)<<std::endl;
+  return 0;
+
+
+
+}
